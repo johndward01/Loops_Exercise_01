0d92d8888fa57b2c2bd2dcc7206bcca31289ccd5
johndward01 <johndward01@gmail.com>
2023-05-19 22:14:55 -0500

    Add project files.

---
+++ Loops_Exercise_01/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+// TODO: Create a method that takes a string as input and prints the string in reverse order using a loop.
+
+
+// TODO: Create a method that print all of the odd numbers below 1000
+
+
+// TODO: Create a method that takes a number from the user and returns the number of digits in that number
+
+
+// TODO: Create a method that takes a string and a character and returns the number of times that character appears in the string
+
+
+// TODO: Create a method that takes a number and calculates the sum of it's digits
+
+
+
+// Bonus:  Create a method that prints a patern.
+// Use a loop and a nested loop to print the following pattern:
+// (ignore the mult-line comment symbols /* */ )
+/*
+
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
