378bd88a3cb21a40ad27a5a65a5cd267d15230cb
johndward01 <johndward01@gmail.com>
2023-05-22 13:14:25 -0500

    update

--- Loops_Exercise_01/Program.cs
+++ Loops_Exercise_01/Program.cs
@@ -1,13 +1,46 @@
-﻿
+﻿// CALL YOUR METHODS HERE
+
+//Console.WriteLine(Reverse("Hello, World!"));
+//PrintOddsBelow1000();
+//Console.WriteLine(NumberOfDigits(1111111111));
+
+
 
 // TODO: Create a method that takes a string as input and prints the string in reverse order using a loop.
+static string Reverse(string s)
+{
+    var reversed = "";
+    for (int i = s.Length - 1; i >= 0; i--)
+	{
+        reversed += s[i];
+    }
+    return reversed;
+}
 
 
 // TODO: Create a method that print all of the odd numbers below 1000
+static void PrintOddsBelow1000()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
 
 
 // TODO: Create a method that takes a number from the user and returns the number of digits in that number
-
+static int NumberOfDigits(int number)
+{
+    var count = 0;
+    foreach (var digit in number.ToString())
+    {
+        count++;
+    }
+    return count;
+}
 
 // TODO: Create a method that takes a string and a character and returns the number of times that character appears in the string
 
